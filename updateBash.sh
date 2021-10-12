c1e908ca8a6b1366f59f8758c3aa8be6b43f5f6e
kayle <kayle@qubit.com>
2021-10-12 14:06:02 +0100

    update refs

--- updateBash.sh
+++ updateBash.sh
@@ -3,7 +3,7 @@
 if [ -d ${HOME}/.bash_it ]; then 
   echo THERE IS BASH_IT
 else 
-  cp -r ${HOME}/.quaren/bash_it ${HOME}/.bash_it
+  cp -r ${HOME}/.dot/bash_it ${HOME}/.bash_it
   ~/.bash_it/install.sh
   [ -f ${HOME}/.bashrc ] && sed -i.backup -e "s,THEME='.*',THEME='zork',g" ${HOME}/.bashrc
   [ -f ${HOME}/.bash_profile ] && sed -i.backup -e "s,THEME='.*',THEME='zork',g" ${HOME}/.bash_profile
--- updateTmux.sh
+++ updateTmux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
  [ -f ${HOME}/.tmux.conf ] && mv ${HOME}/.tmux.conf ${HOME}/.tmux.conf.bak
- cp ${HOME}/.quaren/files/tmux.conf ${HOME}/.tmux.conf
- cp -r ${HOME}/.quaren/files/tmux/ ${HOME}/.tmux/
+ cp ${HOME}/.dot/files/tmux.conf ${HOME}/.tmux.conf
+ cp -r ${HOME}/.dot/files/tmux/ ${HOME}/.tmux/
