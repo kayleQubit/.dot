a961a88f7a7fc59fe87d0eb848dbc32852ba23e8
kayleQubit <38135651+kayleQubit@users.noreply.github.com>
2021-12-01 21:06:19 +0000

    Update tmux theme to GH Dark Dimmed Colors

--- updateTmux.sh
+++ updateTmux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/gpakosz/.tmux.git ${HOME}/.tmux
+git clone --depth=1 https://github.com/gpakosz/.tmux.git ${HOME}/.tmux
 ln -s -f ${HOME}/.tmux/.tmux.conf ${HOME}/.tmux.conf
 cp ${HOME}/.dot/files/tmux.conf.local ${HOME}/.tmux.conf.local
